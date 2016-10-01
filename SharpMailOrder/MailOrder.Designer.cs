9db53229207cdd35e37b947a57689d24fcb6e9da
Dilpreet Singh <preet19@outlook.com>
2016-09-30 22:19:11 -0400

    final version

--- SharpMailOrder/MailOrder.Designer.cs
+++ SharpMailOrder/MailOrder.Designer.cs
@@ -38,11 +38,11 @@
             this.hoursWorkedTextBox = new System.Windows.Forms.TextBox();
             this.TotalSalesTextBox = new System.Windows.Forms.TextBox();
             this.SalesBonusTextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.EmployeeNameLabel = new System.Windows.Forms.Label();
+            this.EmployeeIDLabel = new System.Windows.Forms.Label();
+            this.hoursWorkedLabel = new System.Windows.Forms.Label();
+            this.TotalSalesLabel = new System.Windows.Forms.Label();
+            this.SalesBonusLabel = new System.Windows.Forms.Label();
             this.CalculateBMIButton = new System.Windows.Forms.Button();
             this.PrintMessageButton = new System.Windows.Forms.Button();
             this.ClearFormButton = new System.Windows.Forms.Button();
@@ -82,6 +82,7 @@
             this.FrenchLanguageRadioButton.TabIndex = 0;
             this.FrenchLanguageRadioButton.Text = "Français";
             this.FrenchLanguageRadioButton.UseVisualStyleBackColor = true;
+            this.FrenchLanguageRadioButton.CheckedChanged += new System.EventHandler(this.FrenchLanguageRadioButton_CheckedChanged);
             // 
             // EnglishLanguageRadioButton
             // 
@@ -94,6 +95,7 @@
             this.EnglishLanguageRadioButton.TabStop = true;
             this.EnglishLanguageRadioButton.Text = "English";
             this.EnglishLanguageRadioButton.UseVisualStyleBackColor = true;
+            this.EnglishLanguageRadioButton.CheckedChanged += new System.EventHandler(this.EnglishLanguageRadioButton_CheckedChanged);
             // 
             // EmployeeNametextBox
             // 
@@ -134,55 +136,62 @@
             this.SalesBonusTextBox.Size = new System.Drawing.Size(100, 20);
             this.SalesBonusTextBox.TabIndex = 6;
             // 
-            // label1
+            // EmployeeNameLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(114, 158);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Employee\'s Name";
+            this.EmployeeNameLabel.AutoSize = true;
+            this.EmployeeNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeeNameLabel.Location = new System.Drawing.Point(87, 158);
+            this.EmployeeNameLabel.Name = "EmployeeNameLabel";
+            this.EmployeeNameLabel.Size = new System.Drawing.Size(135, 16);
+            this.EmployeeNameLabel.TabIndex = 7;
+            this.EmployeeNameLabel.Text = "Employee\'s Name";
             // 
-            // label2
+            // EmployeeIDLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(114, 199);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Employee ID";
+            this.EmployeeIDLabel.AutoSize = true;
+            this.EmployeeIDLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmployeeIDLabel.Location = new System.Drawing.Point(87, 199);
+            this.EmployeeIDLabel.Name = "EmployeeIDLabel";
+            this.EmployeeIDLabel.Size = new System.Drawing.Size(97, 16);
+            this.EmployeeIDLabel.TabIndex = 8;
+            this.EmployeeIDLabel.Text = "Employee ID";
             // 
-            // label3
+            // hoursWorkedLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(114, 244);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Hours Worked";
+            this.hoursWorkedLabel.AutoSize = true;
+            this.hoursWorkedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursWorkedLabel.Location = new System.Drawing.Point(87, 244);
+            this.hoursWorkedLabel.Name = "hoursWorkedLabel";
+            this.hoursWorkedLabel.Size = new System.Drawing.Size(107, 16);
+            this.hoursWorkedLabel.TabIndex = 9;
+            this.hoursWorkedLabel.Text = "Hours Worked";
+            this.hoursWorkedLabel.Click += new System.EventHandler(this.hoursWorkedLabel_Click);
             // 
-            // label4
+            // TotalSalesLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(114, 283);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "Total Sales";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
+            this.TotalSalesLabel.AutoSize = true;
+            this.TotalSalesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalSalesLabel.Location = new System.Drawing.Point(87, 283);
+            this.TotalSalesLabel.Name = "TotalSalesLabel";
+            this.TotalSalesLabel.Size = new System.Drawing.Size(88, 16);
+            this.TotalSalesLabel.TabIndex = 10;
+            this.TotalSalesLabel.Text = "Total Sales";
+            this.TotalSalesLabel.Click += new System.EventHandler(this.label4_Click);
             // 
-            // label5
+            // SalesBonusLabel
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(114, 324);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(66, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "Sales Bonus";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
+            this.SalesBonusLabel.AutoSize = true;
+            this.SalesBonusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SalesBonusLabel.Location = new System.Drawing.Point(87, 324);
+            this.SalesBonusLabel.Name = "SalesBonusLabel";
+            this.SalesBonusLabel.Size = new System.Drawing.Size(95, 16);
+            this.SalesBonusLabel.TabIndex = 11;
+            this.SalesBonusLabel.Text = "Sales Bonus";
+            this.SalesBonusLabel.Click += new System.EventHandler(this.label5_Click);
             // 
             // CalculateBMIButton
             // 
+            this.CalculateBMIButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CalculateBMIButton.Location = new System.Drawing.Point(29, 364);
             this.CalculateBMIButton.Name = "CalculateBMIButton";
             this.CalculateBMIButton.Size = new System.Drawing.Size(92, 32);
@@ -193,20 +202,23 @@
             // 
             // PrintMessageButton
             // 
+            this.PrintMessageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PrintMessageButton.Location = new System.Drawing.Point(174, 364);
             this.PrintMessageButton.Name = "PrintMessageButton";
             this.PrintMessageButton.Size = new System.Drawing.Size(87, 32);
             this.PrintMessageButton.TabIndex = 13;
             this.PrintMessageButton.Text = "Print";
             this.PrintMessageButton.UseVisualStyleBackColor = true;
+            this.PrintMessageButton.Click += new System.EventHandler(this.PrintMessageButton_Click);
             // 
             // ClearFormButton
             // 
+            this.ClearFormButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ClearFormButton.Location = new System.Drawing.Point(310, 364);
             this.ClearFormButton.Name = "ClearFormButton";
             this.ClearFormButton.Size = new System.Drawing.Size(88, 32);
             this.ClearFormButton.TabIndex = 14;
-            this.ClearFormButton.Text = "Next";
+            this.ClearFormButton.Text = "Clear";
             this.ClearFormButton.UseVisualStyleBackColor = true;
             this.ClearFormButton.Click += new System.EventHandler(this.ClearFormButton_Click);
             // 
@@ -218,11 +230,11 @@
             this.Controls.Add(this.ClearFormButton);
             this.Controls.Add(this.PrintMessageButton);
             this.Controls.Add(this.CalculateBMIButton);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SalesBonusLabel);
+            this.Controls.Add(this.TotalSalesLabel);
+            this.Controls.Add(this.hoursWorkedLabel);
+            this.Controls.Add(this.EmployeeIDLabel);
+            this.Controls.Add(this.EmployeeNameLabel);
             this.Controls.Add(this.SalesBonusTextBox);
             this.Controls.Add(this.TotalSalesTextBox);
             this.Controls.Add(this.hoursWorkedTextBox);
@@ -251,11 +263,11 @@
         private System.Windows.Forms.TextBox hoursWorkedTextBox;
         private System.Windows.Forms.TextBox TotalSalesTextBox;
         private System.Windows.Forms.TextBox SalesBonusTextBox;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label EmployeeNameLabel;
+        private System.Windows.Forms.Label EmployeeIDLabel;
+        private System.Windows.Forms.Label hoursWorkedLabel;
+        private System.Windows.Forms.Label TotalSalesLabel;
+        private System.Windows.Forms.Label SalesBonusLabel;
         private System.Windows.Forms.Button CalculateBMIButton;
         private System.Windows.Forms.Button PrintMessageButton;
         private System.Windows.Forms.Button ClearFormButton;
--- SharpMailOrder/MailOrder.cs
+++ SharpMailOrder/MailOrder.cs
@@ -42,9 +42,9 @@ namespace SharpMailOrder
             // it checks if input integer value is not greater then 160
             if (Convert.ToInt32(hoursWorkedTextBox.Text) > 160)
             {
-                MessageBox.Show("Please provide valid number only smaller then 150");
-                
+                MessageBox.Show("Please provide valid number only smaller then 160");
             }
+            
         }
 
         private void label4_Click(object sender, EventArgs e)
@@ -67,21 +67,81 @@ namespace SharpMailOrder
 
         }
 
+        //  calculate total sales bonus
         private void CalculateBMIButton_Click(object sender, EventArgs e)
         {
-
-            var hoursWorkedPercentage = Convert.ToInt32(hoursWorkedTextBox.Text) / 160;
-            var totalBonusAmount = Convert.ToInt32(TotalSalesTextBox.Text) * 0.02;
-            var salesBonus = hoursWorkedPercentage * totalBonusAmount;
-            SalesBonusTextBox.Text = Convert.ToString( salesBonus);
+            if (EmployeeNametextBox.Text.Length > 0 && EmployeeIDtextBox.Text.Length > 0 &&
+                hoursWorkedTextBox.Text.Length > 0 )
+            {
+                // get hours worked and divide by 160
+                var hoursWorkedPercentage = Convert.ToInt32(hoursWorkedTextBox.Text) / 160;
+                // get total bonus and multiple by 0.02
+                var totalBonusAmount = Convert.ToInt32(TotalSalesTextBox.Text) * 0.02;
+                // finally for sales bonus multiply both fields
+                var salesBonus = hoursWorkedPercentage * totalBonusAmount;
+                // write the sale bonus to sales bonus text field
+                SalesBonusTextBox.Text = Convert.ToString(salesBonus);
+            }
+            else
+            {
+                MessageBox.Show("please fill all text values");
+            }
         }
+
+        // clear the form
 
         private void ClearFormButton_Click(object sender, EventArgs e)
         {
+            // empty the emplayee name field
             EmployeeNametextBox.Text = String.Empty;
+            // empty the emplayee id field
             EmployeeIDtextBox.Text = String.Empty;
            // hoursWorkedTextBox.Text = String.Empty;
+           // empty the sales bonus field
             SalesBonusTextBox.Text = String.Empty;
+        }
+
+        // print the message to screen
+
+        private void PrintMessageButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("form is being sent to the printer");
+        }
+
+        // radio button to convert the form to english language
+        private void EnglishLanguageRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (EnglishLanguageRadioButton.Checked)
+            {
+                // changes the label values to english
+                EmployeeNameLabel.Text = "Employee Name";
+                EmployeeIDLabel.Text = "Employee ID";
+                hoursWorkedLabel.Text = "Hours Worked";
+                TotalSalesLabel.Text = "Total Sales";
+                SalesBonusLabel.Text = "Sales Bonus";
+                CalculateBMIButton.Text = "Calculate";
+                PrintMessageButton.Text = "Print";
+                ClearFormButton.Text = "Clear";
+            }
+        }
+
+        // radio button to convert the form to french language
+        private void FrenchLanguageRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            // change the label values to french
+            EmployeeNameLabel.Text = "Nom de l'employé";
+            EmployeeIDLabel.Text = "employé ID";
+            hoursWorkedLabel.Text = "Heures travaillées";
+            TotalSalesLabel.Text = "Ventes totales";
+            SalesBonusLabel.Text = "Bonus de vente";
+            CalculateBMIButton.Text = "Calculer";
+            PrintMessageButton.Text = "Impression";
+            ClearFormButton.Text = "Clair";
+        }
+
+        private void hoursWorkedLabel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
